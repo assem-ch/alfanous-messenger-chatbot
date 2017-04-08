f04356235b56e85056afa77173763a7a2aa1d3a6
Assem Chelli <assem.ch@gmail.com>
2017-04-08 04:07:24 +0100

    simplify

--- app.js
+++ app.js
@@ -62,8 +62,6 @@ function receivedMessage(event) {
     }
   } else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
-  } else if (event.postback) {
-    receivedPostback(event);
   }
 }
 
@@ -150,22 +148,6 @@ function callSendAPI(messageData) {
   });
 }
 
-function receivedPostback(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfPostback = event.timestamp;
-
-  // The 'payload' param is a developer-defined field which is set in a postback
-  // button for Structured Messages.
-  var payload = event.postback.payload;
-
-  console.log("Received postback for user %d and page %d with payload '%s' " +
-      "at %d", senderID, recipientID, payload, timeOfPostback);
-
-  // When a postback is called, we'll send a message back to the sender to
-  // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
-}
 
 
 app.get('/policy', function (req, res){
